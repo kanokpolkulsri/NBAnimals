4b80a6674cbfaa435c1a259c0805ae87cceacf17
tonplamm <tonplamm@10.10.1.198>
2016-11-17 23:45:09 +0700

    Sound comes out!

--- core/src/com/mygdx/game/GroundPlayer.java
+++ core/src/com/mygdx/game/GroundPlayer.java
@@ -1,5 +1,7 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
@@ -8,11 +10,15 @@ public class GroundPlayer {
 	SpriteBatch batch;
 	BasGame basGame;
 	Texture ground;
+	Music bgSound;
 	
 	public GroundPlayer(BasGame basGame) {
 		this.basGame = basGame;
 		batch = basGame.batch;
 		ground = new Texture("floor.png");
+		bgSound = Gdx.audio.newMusic(Gdx.files.internal("bgSound.mp3"));
+		bgSound.setLooping(true);
+		bgSound.play();
 	}
 	
 	public void render() {
